1944a9b6fd759bf9c18a6e145c19360968b1fb8c
aaronnhan <aaronnhan11@gmail.com>
2019-08-01 19:27:51 -0700

    working commit

---
+++ groupcreator/group.py
@@ -0,0 +1,11 @@
+class Group:
+    def __init__(self, people):
+        self.my_people = people
+    def get_hatred(self):
+        hatred = 0;
+        for person in self.my_people:
+            for other_person in self.my_people:
+                if other_person != person:
+                    hatred += person.get_hatred(other_person)
+        print(hatred)
+        return hatred
